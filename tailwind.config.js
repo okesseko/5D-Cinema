cf1e689350bd4bfb84b9e81eee86af6f70372e3a
okesseko <59650459+okesseko@users.noreply.github.com>
2021-08-26 00:33:27 +0800

    設定基本 css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBlue: "#2481A6",
+        lightBlue: "#00DDF2",
+        gray: "#A6A6A6",
+      },
+      spacing: {},
+    },
   },
   variants: {
     extend: {},
