3bfee4589022f29b48d5b6994d9dcda2a484b862
okesseko <59650459+okesseko@users.noreply.github.com>
2021-08-25 22:30:37 +0800

    初始化設定

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Head from 'next/head';
-import 'tailwindcss/tailwind.css';
+import React from "react";
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
 
 export default function Home() {
   return (
@@ -12,15 +12,11 @@ export default function Home() {
 
       <main>
         <h1 className="title bg-red-500">
-          Welcome to
-          {' '}
-          <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
-          Get started by editing
-          {' '}
-          <code>pages/index.js</code>
+          Get started by editing <code>pages/index.js</code>
         </p>
 
         <div className="grid">
@@ -34,7 +30,10 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className="card">
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className="card"
+          >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -44,7 +43,9 @@ export default function Home() {
             className="card"
           >
             <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
           </a>
         </div>
       </main>
@@ -55,9 +56,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by
-          {' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
     </div>
---
+++ pages/introduce/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InTroduce = () => {
+  return <div>introduce</div>;
+};
+
+export default InTroduce;
