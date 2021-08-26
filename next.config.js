9a2ee55799a2481063f94c7ed98b046f11d814bc
okesseko <59650459+okesseko@users.noreply.github.com>
2021-08-26 21:51:43 +0800

    設定部屬檔案

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 
 module.exports = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
