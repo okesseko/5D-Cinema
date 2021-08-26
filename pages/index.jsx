c4e2cbc93ffc165f7121d21d74e62b7672642a49
okesseko <59650459+okesseko@users.noreply.github.com>
2021-08-26 21:53:33 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -55,7 +55,8 @@ export default function Home() {
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        >    
+        
           Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
