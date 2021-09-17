b1071009c2aaeee8e793e1e4b645e9e297959231
okesseko <59650459+okesseko@users.noreply.github.com>
2021-09-17 18:18:09 +0800

    [page]新增introduce頁面

--- src/pages/introduce/index.jsx
+++ src/pages/introduce/index.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import style from "./introduce.module.scss";
+import autoprefixer from "autoprefixer";
+const IMAGE_SRC = ["/the-flash.jpg", "/maze-runner.jpg", "/spider-man.jpg"];
 
-const InTroduce = () => {
-  return <div>introduce</div>;
+const Introduce = () => {
+  const [imageIndex, setImageIndex] = useState(0);
+  useEffect(() => {
+    const autoChangeImage = setInterval(() => {
+      setImageIndex((imageIndex) => {
+        if (imageIndex + 1 === IMAGE_SRC.length) return 0;
+        else return imageIndex + 1;
+      });
+    }, 5000);
+    return () => {
+      clearInterval(autoChangeImage);
+    };
+  }, []);
+  return (
+    <div className={style.introduce}>
+      <div className={style.carousel}>
+        <div className={`${style.side_select} ${style.right}`}>
+          <Image
+            onClick={() => {
+              if (imageIndex - 1 === -1) setImageIndex(IMAGE_SRC.length - 1);
+              else setImageIndex(imageIndex - 1);
+            }}
+            src="/next_icon.svg"
+            width={80}
+            height={100}
+          />
+        </div>
+        <Image src={IMAGE_SRC[imageIndex]} layout="fill" objectFit="cover" />
+        <div className={`${style.side_select} ${style.left}`}>
+          <Image
+            onClick={() => {
+              if (imageIndex + 1 === IMAGE_SRC.length) setImageIndex(0);
+              else setImageIndex(imageIndex + 1);
+            }}
+            src="/next_icon.svg"
+            width={80}
+            height={100}
+          />
+        </div>
+        <div className={style.bottom_dot}>
+          {IMAGE_SRC.map((_, index) => (
+            <span
+              key={index}
+              className={`${style.dot} ${
+                imageIndex === index ? style.active : ""
+              }`}
+              onClick={() => setImageIndex(index)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default InTroduce;
+export default Introduce;
