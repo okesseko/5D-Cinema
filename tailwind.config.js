942752debce5febfc018b2535ee48b987b0f73ca
okesseko <59650459+okesseko@users.noreply.github.com>
2021-09-17 18:17:18 +0800

    [tailwind]新增客製化參數

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,12 @@ module.exports = {
         lightBlue: "#00DDF2",
         gray: "#A6A6A6",
       },
+      scale: {
+        "c-1": "-1",
+      },
       spacing: {
         "c-100": "100px",
+        "c-200": "200px",
         "c-450": "450px",
       },
     },
