d02230928995d77a7b195d8ca349cfbb97c4cefc
okesseko <59650459+okesseko@users.noreply.github.com>
2021-09-30 23:53:17 +0800

    page: reserve page

--- src/pages/reserve/index.jsx
+++ src/pages/reserve/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import style from "./reserve.module.scss";
 
 const Reserve = () => {
-  return <div>Reserve</div>;
+  return (
+    <div className={style.reserve}>
+      <div className={style.figure} />
+      <form className={style.form}></form>
+      <ul className={style.stepper}></ul>
+    </div>
+  );
 };
 
 export default Reserve;
