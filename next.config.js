03af3c5b400138e2995b926677998d817755be61
okesseko <59650459+okesseko@users.noreply.github.com>
2022-02-16 23:02:40 +0800

    [deploy]: deploy setting

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ const path = require("path");
 
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  trailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    loader: "custom",
+    path: "/",
   },
 };
